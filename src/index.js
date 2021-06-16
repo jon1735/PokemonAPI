a1af1fd2b117655f29c967907693b4bf3f5ec2cc
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-06-16 15:49:17 -0400

    updating first and second deliverables

--- src/index.js
+++ src/index.js
@@ -10,11 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
       .then(resp => resp.json())
       .then(data => {
         const newPokemonList = document.createElement("li")
+        newPokemonList.classList.add("centered")
         newPokemonList.innerHTML = data.name
         const newPokemonImg = document.createElement("img")
         newPokemonImg.src = data.sprites.front_default
         newPokemonList.append(newPokemonImg)
         pokemonList.append(newPokemonList)
+        // add the event listener here
+        // add the fetch after the EL
+        // need pokemon name, need elemend id
+        newPokemonImg.addEventListener("click", (event) => {
+          event.preventDefault()
+          const pokemonAbilities = document.querySelector("#ability-list")
+          fetch(`https://pokeapi.co/api/v2/pokemon/${searchPokemon}`)
+            .then(resp => resp.json())
+            .then(data => {
+              data.abilities.forEach(ability => {
+                const newPokemonAbilities = document.createElement("li")
+                newPokemonAbilities.innerHTML = ability.ability.name
+                pokemonAbilities.append(newPokemonAbilities)
+              })
+            })
+        })
       })
   })
 })
